a1dca43382dfa3602c82f8ae1315bed96a855463
MacbethShaw <1481796592@qq.com>
2021-03-25 22:27:59 +0800

    gdhw2: part4

---
+++ gd-hw2/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> platforms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float spawnTime;
+    private float countTime;
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime;
+        if (countTime >= spawnTime)
+        {
+            SpawnPlatform();
+            countTime = 0;
+        }
+    }
+
+    public void SpawnPlatform()
+    {
+        Vector3 spawnPosition = transform.position;
+        spawnPosition.x = Random.Range(-3.5f, 3.5f);
+
+        int index = Random.Range(0, platforms.Count);
+        GameObject go = Instantiate(platforms[index], spawnPosition, Quaternion.identity);
+    }
+}
