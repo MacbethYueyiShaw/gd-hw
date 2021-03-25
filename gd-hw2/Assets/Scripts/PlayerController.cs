408e6548a1268ac750d3cf71fcf4b2a9cd409f2f
MacbethShaw <1481796592@qq.com>
2021-03-26 00:13:57 +0800

    gdhw2: part6

--- gd-hw2/Assets/Scripts/PlayerController.cs
+++ gd-hw2/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
+    Animator animator;
     Rigidbody2D rb;
     public float speed;
+    public float checkRadius;
+    public LayerMask platformMask;
+    public GameObject groundCheck;
+    public bool isOnGround;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.transform.position, checkRadius, platformMask);
+        animator.SetBool("isOnGround", isOnGround);
         Move();
     }
 
@@ -24,11 +33,13 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
         if (xInput != 0)
             transform.localScale = new Vector3(xInput, 1, 1);
+        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     }
 
     public void Die()
     {
         Debug.Log("Die");
+        animator.SetBool("isdead", true);
         GameManager.GameOver(true);
     }
 
@@ -38,5 +49,11 @@ public class PlayerController : MonoBehaviour
         {
             Die();
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, checkRadius);
     }
 }
