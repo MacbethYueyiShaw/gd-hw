c9ca769a37f7a2138e5608bb18458b07f06f958d
MacbethShaw <1481796592@qq.com>
2021-03-25 21:30:47 +0800

    gdhw3:basic player

---
+++ gd-hw2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        if (xInput != 0)
+            transform.localScale = new Vector3(xInput, 1, 1);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Spike"))
+        {
+            Die();
+        }
+    }
+}
