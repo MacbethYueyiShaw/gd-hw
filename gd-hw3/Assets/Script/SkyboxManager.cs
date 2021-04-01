c7068573fff451b6bfd7709ede4d9d4073ab215a
MikeShaw1 <1481796592@qq.com>
2021-04-02 00:28:46 +0800

    hw3:debug

---
+++ gd-hw3/Assets/Script/SkyboxManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+    public Material[] Skyboxes;
+    public float RotationPerSecond = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = Skyboxes[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
+    }
+    public void ChangeSkybox()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+}
