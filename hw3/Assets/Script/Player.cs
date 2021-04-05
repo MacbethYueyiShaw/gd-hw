e34a78242e962e34224c8fe8d1f24fd94ef89b13
MikeShaw1 <1481796592@qq.com>
2021-04-05 16:03:54 +0800

    hw3: add mana bar

--- hw3/Assets/Script/Player.cs
+++ hw3/Assets/Script/Player.cs
@@ -5,31 +5,61 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-	public int maxHealth = 100;
-	public int currentHealth;
+	public float maxHealth = 100f;
+	public float currentHealth;
+    public float maxMana = 100f;
+    public float currentMana;
+    public float manaRecovery = 1f;
 
-	public HealthBar healthBar;
+    public HealthBar healthBar;
+    public ManaBar manaBar;
 
     // Start is called before the first frame update
     void Start()
     {
 		currentHealth = maxHealth;
+        currentMana = maxMana;
 		healthBar.SetMaxHealth(maxHealth);
+        manaBar.SetMaxMana(maxMana);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			TakeDamage(20);
-		}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20f);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UseSkill(20f);
+        }
+        if (currentMana<=maxMana)
+        {
+            currentMana += manaRecovery * Time.deltaTime;
+            if(currentMana > maxMana)
+            {
+                currentMana = maxMana;
+            }
+            manaBar.SetMana(currentMana);
+        }
     }
 
-	void TakeDamage(int damage)
+    void TakeDamage(float damage)
 	{
 		currentHealth -= damage;
 
 		healthBar.SetHealth(currentHealth);
 	}
+    void UseSkill(float manaCost)
+    {
+        if (currentMana < manaCost)
+        {
+            Debug.Log("Mana not enough!");
+            return;
+        }
+        currentMana -= manaCost;
+
+        manaBar.SetMana(currentMana);
+    }
 }
