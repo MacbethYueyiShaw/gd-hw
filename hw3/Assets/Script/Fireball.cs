0688837b919773d03e974a04b8dd743870074a08
MikeShaw1 <1481796592@qq.com>
2021-04-09 18:20:02 +0800

    hw3: add shoot

---
+++ hw3/Assets/Script/Fireball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public GameObject effect;
+    float existTime = 5f;
+    float currentTime = 0f;
+
+    void Update()
+    {
+        if (currentTime>=existTime)
+        {
+            Destroy(gameObject);
+        }
+        currentTime += Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.name != "player")
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
+    }
+}
