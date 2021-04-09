a0394bdbe4495308155004a5ce9f846e4e6f8890
MikeShaw1 <1481796592@qq.com>
2021-04-09 16:12:09 +0800

    hw3: update firepoint

---
+++ hw3/Assets/FirePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePoint : MonoBehaviour
+{
+    public Camera cam;
+    public Rigidbody2D follow;
+    public Rigidbody2D rb;
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset;
+        offset.x = 0f;
+        offset.y = -0.1f;
+        rb.position = follow.position+offset;
+        //Debug.Log(Input.mousePosition);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(mousePos);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        rb.rotation = angle;
+    }
+}
