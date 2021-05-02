3f1b54c53fdf1f3c9a85624287fb9672561346c1
MacbethShaw <1481796592@qq.com>
2021-05-02 22:10:55 +0800

    hw5: gamemanager

---
+++ hw5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("NextLevel"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex < 2)
+            {
+                //Debug.Log(SceneManager.GetActiveScene().buildIndex);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+            
+        }
+    }
+}
