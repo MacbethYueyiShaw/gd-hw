112dc05d2c610c15de90b885665997ae50decbf0
MikeShaw1 <1481796592@qq.com>
2021-05-20 00:57:12 +0800

    hw8:hair strand slider

--- hw8/Assets/Scripts/HairStrand.cs
+++ hw8/Assets/Scripts/HairStrand.cs
@@ -1,28 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HairStrand : MonoBehaviour
 {
     [SerializeField] GameObject hair;
+    [SerializeField] Slider slider;
     [SerializeField] int strand_number = 33;
+     private bool isInit = false;
     [SerializeField] Transform head;
     [SerializeField] float head_radius = 0.5f;
     [SerializeField] List<GameObject> hairs = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
+        AddHair(strand_number);
+        isInit = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void AddHair(int value)
+    {
         float alpha = 90f;
         float beta = 0f;
         float delta = 15f;
-        float d2r =1/ 180f * Mathf.PI;
+        float d2r = 1 / 180f * Mathf.PI;
         int loop_times = 0;
         int iterations = 12;
 
-        int tmp_size = strand_number;
+        int tmp_size = value;
         int counter = 0;
         int remain = 0;
-        while (tmp_size>=0)
+        while (tmp_size >= 0)
         {
             //Debug.Log(counter);
             remain = tmp_size;
@@ -31,12 +46,13 @@ public class HairStrand : MonoBehaviour
             counter++;
         }
 
-       /* Debug.Log(remain);
-        Debug.Log(loop_times);*/
+        /* Debug.Log(remain);
+         Debug.Log(loop_times);*/
+        int index = 0;
 
         for (int j = 0; j < loop_times; j++)
         {
-            if (j == loop_times-1)
+            if (j == loop_times - 1)
             {
                 iterations = remain;
             }
@@ -45,6 +61,7 @@ public class HairStrand : MonoBehaviour
 
             for (int i = 0; i < iterations; i++)
             {
+                if (index++ < strand_number && isInit) continue;
                 float x = head_radius * Mathf.Sin(alpha * d2r) * Mathf.Cos((beta + i * delta) * d2r);
                 float z = head_radius * Mathf.Sin(alpha * d2r) * Mathf.Sin((beta + i * delta) * d2r);
                 float y = head_radius * Mathf.Cos(alpha * d2r);
@@ -66,11 +83,26 @@ public class HairStrand : MonoBehaviour
             beta += 7.5f;
             iterations--;
         }
+        strand_number = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void editSize()
     {
-        
+        int value = (int)slider.value;
+        if (value == strand_number) return;
+        else if (value > strand_number)
+        {
+            AddHair(value);
+        }
+        else if (value < strand_number)
+        {
+            for(int i = strand_number; i > value; i--)
+            {
+                if (i == 0) return;
+                Destroy(hairs[i - 1]);
+                hairs.Remove(hairs[i-1]);
+            }
+            strand_number = value;
+        } 
     }
 }
